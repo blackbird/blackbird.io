f1b10071db92f12f87119baf5032a0d0c1eb3c6d
SCDevy <hello@stephenc.me>
2016-12-02 13:24:09 -0800

    Adds project info, fixes project ordering

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,20 @@
-function scrollTo(elem, event) {
+var BREAKPOINT = 1024;
+
+var isSwapped = false;
+var techlaSwap = [document.getElementById('techla-1'), document.getElementById('techla-2')];
+
+// Helper functions
+getViewportWidth = function() {
+	return Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+};
+
+swapElementInnerHTML = function(elemRefA, elemRefB) {
+	var temp = elemRefA.innerHTML;
+	elemRefA.innerHTML = elemRefB.innerHTML;
+	elemRefB.innerHTML = temp;
+};
+
+scrollTo = function(elem, event) {
 	event.preventDefault();
 
 	var elemTargetId = elem.href.substr(elem.href.indexOf('#'));
@@ -6,4 +22,19 @@ function scrollTo(elem, event) {
 	document.querySelector(elemTargetId).scrollIntoView({
 		behavior: 'smooth'
 	});
-}
+};
+
+// Primary functions
+(function() {
+	if(getViewportWidth() < BREAKPOINT) {
+		swapElementInnerHTML(techlaSwap[0], techlaSwap[1]);
+		isSwapped = !isSwapped;
+	}
+})();
+
+window.onresize = function() {	
+	if((getViewportWidth() < BREAKPOINT && !isSwapped) || (getViewportWidth() >= BREAKPOINT && isSwapped)) {
+		swapElementInnerHTML(techlaSwap[0], techlaSwap[1]);
+		isSwapped = !isSwapped;
+	}
+};
