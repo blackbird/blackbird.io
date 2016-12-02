4058f2313ebe9e976e5a0098021614be80e8ca15
SCDevy <hello@stephenc.me>
2016-12-02 14:54:03 -0800

    First version of new BB site

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,7 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
+
+git subtree add --prefix dist git@github.com:blackbird/blackbirdstudios.io.git master --squash
+
 git subtree push --prefix=public git@github.com:blackbird/blackbirdstudios.io.git gh-pages
